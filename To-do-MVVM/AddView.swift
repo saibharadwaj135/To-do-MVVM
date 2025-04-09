e83ee45d67eb86a82bdcd4137c6a793d0bac567a
saibharadwaj135 <saibharadwaj135@gmail.com>
2025-04-09 18:41:48 -0400

    -Built ToDO list app with MVVM architecture - used user Defaults to store the data - used animations for the add something option

---
+++ To-do-MVVM/AddView.swift
@@ -0,0 +1,18 @@
+//
+//  AddView.swift
+//  To-do-MVVM
+//
+//  Created by Sai bharadwaj Adapa on 4/9/25.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    AddView()
+}
---
+++ To-do-MVVM/ItemModel.swift
@@ -0,0 +1,8 @@
+//
+//  ItemModel.swift
+//  To-do-MVVM
+//
+//  Created by Sai bharadwaj Adapa on 4/9/25.
+//
+
+import Foundation
