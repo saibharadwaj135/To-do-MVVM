98b9ca2582d4c1ffbe280fe37b9b0200e1b24d8a
saibharadwaj135 <saibharadwaj135@gmail.com>
2025-04-09 15:51:45 -0400

    Initial Commit

---
+++ To-do-MVVM/To_do_MVVMApp.swift
@@ -0,0 +1,17 @@
+//
+//  To_do_MVVMApp.swift
+//  To-do-MVVM
+//
+//  Created by Sai bharadwaj Adapa on 4/9/25.
+//
+
+import SwiftUI
+
+@main
+struct To_do_MVVMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
